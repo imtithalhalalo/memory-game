3fa6bc20cfaf423e8baff1fa729c1492c883019c
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-16 16:12:37 +0300

    check first click or second

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const cards = document.querySelectorAll('.memory-card')
 const score = document.getElementById('score')
-
+let flipped = false;
+let first_card, second_card;
 
 cards.forEach(card => card.addEventListener('click', flip = () => {
     card.classList.add('flip')
+    if (!flipped) {
+        flipped = true;
+        first_card = card;
+    } else {
+        flipped = false;
+        second_card = card;
+    }
 }))
 
