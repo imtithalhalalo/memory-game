aefd2e1188de76f16dcb8d9c5086d1df1a99df27
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-16 16:14:27 +0300

    Adding score system to the page

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const cards = document.querySelectorAll('.memory-card')
 const score = document.getElementById('score')
 let flipped = false;
 let first_card, second_card;
-
+let num = 0
 //check if they match
 const match = () => {
     if (first_card.dataset.framework == second_card.dataset.framework) {
         first_card.removeEventListener('click', flip);
         second_card.removeEventListener('click', flip);
+        num += 1
+        score.innerText = `Your score is: ${num}`
     } else {
         setTimeout(() => {
             first_card.classList.remove('flip');
