2dfa0301a9028add6552e7bf740b88eff6f0e836
imtithalhalalo <halaloimtithal@gmail.com>
2022-09-16 16:13:57 +0300

    checking if the second card flipped if it matches the first

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const score = document.getElementById('score')
 let flipped = false;
 let first_card, second_card;
 
+//check if they match
+const match = () => {
+    if (first_card.dataset.framework == second_card.dataset.framework) {
+        first_card.removeEventListener('click', flip);
+        second_card.removeEventListener('click', flip);
+    } else {
+        setTimeout(() => {
+            first_card.classList.remove('flip');
+            second_card.classList.remove('flip');
+        }, 1000);
+    }
+}
 cards.forEach(card => card.addEventListener('click', flip = () => {
     card.classList.add('flip')
     if (!flipped) {
@@ -11,6 +23,7 @@ cards.forEach(card => card.addEventListener('click', flip = () => {
     } else {
         flipped = false;
         second_card = card;
+        match()
     }
 }))
 
